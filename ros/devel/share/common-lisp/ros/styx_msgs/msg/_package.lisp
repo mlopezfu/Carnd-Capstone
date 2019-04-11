91ed17cf75b83f0032e37de12c6d45f50f6a0ab5
Miguel <mlopezfu@gmail.com>
2019-04-11 09:39:54 -0700

    change after review

---
+++ ros/devel/share/common-lisp/ros/styx_msgs/msg/_package.lisp
@@ -0,0 +1,15 @@
+(cl:defpackage styx_msgs-msg
+  (:use )
+  (:export
+   "<LANE>"
+   "LANE"
+   "<TLSTATUS>"
+   "TLSTATUS"
+   "<TRAFFICLIGHT>"
+   "TRAFFICLIGHT"
+   "<TRAFFICLIGHTARRAY>"
+   "TRAFFICLIGHTARRAY"
+   "<WAYPOINT>"
+   "WAYPOINT"
+  ))
+
---
+++ ros/devel/share/gennodejs/ros/styx_msgs/msg/_index.js
@@ -0,0 +1,16 @@
+
+"use strict";
+
+let TrafficLightArray = require('./TrafficLightArray.js');
+let Waypoint = require('./Waypoint.js');
+let TrafficLight = require('./TrafficLight.js');
+let TLStatus = require('./TLStatus.js');
+let Lane = require('./Lane.js');
+
+module.exports = {
+  TrafficLightArray: TrafficLightArray,
+  Waypoint: Waypoint,
+  TrafficLight: TrafficLight,
+  TLStatus: TLStatus,
+  Lane: Lane,
+};
